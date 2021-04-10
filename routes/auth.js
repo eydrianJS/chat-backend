33a07dfeb4b2395db8618753ec96bba4ddc7e2dc
Adrian Olszowski <aol@spyro-soft.com>
2021-04-10 13:22:04 +0200

    post

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const AuthController = require('../controllers/AuthController');
 
 router.post('/register', AuthController.register);
-router.get('/login', AuthController.login);
+router.post('/login', AuthController.login);
 
 module.exports = router;
